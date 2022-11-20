ccd01a72960d74e26f8a1c82f3b944dfe9237a02
Marierlin Higuerey <67464628+Marierlinh@users.noreply.github.com>
2022-11-20 10:56:52 -0500

    style

--- globe_bank/public/staff/index.php
+++ globe_bank/public/staff/index.php
@@ -1,9 +1,25 @@
-<?php require_once('../../private/initialize.php'); ?>
+<!DOCTYPE html>
 
-<?php $page_title = 'Staff Menu'; ?>
-<?php include(SHARED_PATH . '/staff_header.php'); ?>
-
-<div id="content">
-</div>
-
-<?php include(SHARED_PATH . '/staff_footer.php'); ?>
+<html>
+    <head>
+        <title>MHR</title>
+        <meta charset="utf-8">
+        <link  rel="stylesheet" media="all" href="../stylesheets/staff.css"/>
+    </head>
+    <body>
+        <header>
+            <h1>MHR Staff Area</h1>
+        </header>
+        <navigation>
+            <ul>
+                <li><a href="index.php">Menu</a></li>
+            </ul>
+        </navigation>
+        <div id="content">
+            
+        </div>
+        <footer>
+            &copy; <?php echo date("Y");?> Globe Bank
+        </footer>
+    </body>
+</html>
