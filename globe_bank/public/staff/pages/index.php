b089486a0aeb2c76c5bd4c105f94f9f3ee5a71c2
Marierlin Higuerey <67464628+Marierlinh@users.noreply.github.com>
2022-11-21 19:55:21 -0500

    URL parameters

--- globe_bank/public/staff/pages/index.php
+++ globe_bank/public/staff/pages/index.php
@@ -4,24 +4,12 @@
   <head>
     <title>MHR</title>
     <meta charset="utf-8">
-    <link  rel="stylesheet" media="all" href="../stylesheets/staff.css"/>
+    
   </head>
 
   <body>
-    
-    <header>
-      <h1>MHR Staff Area</h1>
-    </header>
 
-    <navigation>
-      <ul>
-        <li><a href="index.php">Menu</a></li>
-      </ul>
-    </navigation>
-
-    <footer>
-       &copy; <?php echo date("Y");?> Globe Bank
-    </footer>
+   
 
   </body>
 </html>
